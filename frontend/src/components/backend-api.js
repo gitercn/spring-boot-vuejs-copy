6ce4d2fe28823bbc8f9d9fe58e3738d4d8240912
gitercn <53464166+gitercn@users.noreply.github.com>
2020-03-23 12:21:15 +0800

    remove headers' Access-Control-Allow-Origin

--- frontend/src/components/backend-api.js
+++ frontend/src/components/backend-api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const AXIOS = axios.create({
   baseURL: `/`, // 添加了devServer.proxy之后这里也需要从http://localhost:8080改成/
-  headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8080'
-  }
+  // headers: { // 添加了devServer.proxy之后这个header也就不需要了
+  //   'Access-Control-Allow-Origin': 'http://localhost:8080'
+  // }
 })
