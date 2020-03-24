510962c47cb6a9633622ef61af9ec31f2c56c155
gitercn <53464166+gitercn@users.noreply.github.com>
2020-03-24 14:11:40 +0800

    cleaning

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,44 +1,13 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.example.demo.dao.RoomMapper;
 
 @SpringBootApplication
 public class DemoApplication  {
-
-//	private final RoomMapper roomMapper;
-//
-//	public DemoApplication(RoomMapper roomMapper) {
-//	    this.roomMapper = roomMapper;
-//	  }
-
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println(this.roomMapper.findByRoomId("CA"));
-//	}
-
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//	  return new WebMvcConfigurerAdapter() {
-//	    @Override
-//	    public void addCorsMappings(CorsRegistry registry) {
-//	      registry.addMapping("/api/*").allowedOrigins("http://localhost:8080");
-//	    }
-//	  };
-//	}
 
 }
