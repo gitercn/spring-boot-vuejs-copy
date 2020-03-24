06404894aafcf436dbf9eee0c1c7930872801e30
gitercn <53464166+gitercn@users.noreply.github.com>
2020-03-24 14:10:16 +0800

    works with mysql with official demos

---
+++ backend/src/main/java/com/example/demo/vo/RoomVO.java
@@ -0,0 +1,20 @@
+package com.example.demo.vo;
+
+public class RoomVO {
+	
+	private String roomid;
+	private String roomdetail;
+	public String getRoomid() {
+		return roomid;
+	}
+	public void setRoomid(String roomid) {
+		this.roomid = roomid;
+	}
+	public String getRoomdetail() {
+		return roomdetail;
+	}
+	public void setRoomdetail(String roomdetail) {
+		this.roomdetail = roomdetail;
+	}
+
+}
