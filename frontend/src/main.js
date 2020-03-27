138841c9598aafe97140769b09d94b59b3b53a82
gitercn <53464166+gitercn@users.noreply.github.com>
2020-03-27 16:15:14 +0800

    add vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VModal from 'vue-js-modal'
+import vuetify from './plugins/vuetify';
 Vue.use(VModal)
 
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
