b00ccdd53acdabda25ec8cf8926637b8468842dd
gitercn <53464166+gitercn@users.noreply.github.com>
2020-04-07 15:44:23 +0800

    Revert "change backend-api from / to /api to see if heroku works?"
    This reverts commit 12af74e63cf168d60803a92cce39d6bdfc862971.

--- frontend/src/components/backend-api.js
+++ frontend/src/components/backend-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const AXIOS = axios.create({
-  baseURL: `/api`, // 添加了devServer.proxy之后这里也需要从http://localhost:8080改成/
+  baseURL: `/`, // 添加了devServer.proxy之后这里也需要从http://localhost:8080改成/
   // headers: { // 添加了devServer.proxy之后这个header也就不需要了
   //   'Access-Control-Allow-Origin': 'http://localhost:8080'
   // }
