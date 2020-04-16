8df9358e0b5fb232d7b34a19c0e5d0e1e046a17d
gitercn <53464166+gitercn@users.noreply.github.com>
2020-04-16 12:56:22 +0800

    add v-app-bar and v-navigation-drawer

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,13 +1,34 @@
 <template>
   <nav>
     <v-app-bar app>
-
+        <!-- <v-app-bar-title>
+            Title
+        </v-app-bar-title> -->
+        <v-btn @click="drawer = !drawer">Menu</v-btn>
+        <v-toolbar-title>
+            Room Booking
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn>
+            <span>Sign Out</span>
+            <!-- <v-icon right>exit_to_app</v-icon> -->
+        </v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+        <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+    data(){
+        return{
+            drawer: false,
+        }
+    }
+};
 </script>
 
 <style>
