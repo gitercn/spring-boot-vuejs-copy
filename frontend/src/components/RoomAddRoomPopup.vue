d448dad098ff776a4c015e04eedaa4b9a512ca5c
gitercn <53464166+gitercn@users.noreply.github.com>
2020-04-28 13:47:12 +0800

    change button color and set max-width

--- frontend/src/components/RoomAddRoomPopup.vue
+++ frontend/src/components/RoomAddRoomPopup.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-dialog v-model="dialog">
+  <v-dialog v-model="dialog" max-width="600px">
     <template v-slot:activator="{ on }">
-      <v-btn v-on="on">Add Room</v-btn>
+      <v-btn v-on="on" color="primary">Add Room</v-btn>
     </template>
 
     <v-card>
